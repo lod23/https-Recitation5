a98df975c38ba287573f0c6dcdb76ba01792c1fe
lod23 <121730996+lod23@users.noreply.github.com>
2025-02-24 13:00:19 -0500

    initial changes

--- src/main/java/EmailValidator.java
+++ src/main/java/EmailValidator.java
@@ -5,10 +5,34 @@ public class EmailValidator {
     public static final char AT = '@';
 
     // Add your enumerated states after this line
+//    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+//            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+//    );
 
-
-    public static boolean isEmailValid(String address){
+    public static boolean isEmailValid(String address) {
         // TODO: replace this line with your code
+        int count = 0;
+        int totalDomainCount = 0;
+        char ch;
+
+        for (int index = 0; index < address.length(); index++){
+            ch = address.charAt(index);
+
+
+            if (index == 0 && ch == DOT) {
+                return false;
+
+            }
+            //if ch is a DOT and next is also a DOT
+            if (index == 0 && ch == DOT){
+                if(address.charAt(index + 1) == DOT || (address.charAt(index + 1) == AT){
+                    return false;
+                }
+
+            }
+            count++;
+
+        }
         return false;
     }
 }
